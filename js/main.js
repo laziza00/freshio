e78f417a4cad3087747672750677dec5a6490519
laziza@mail.ru <laziza00>
2022-02-25 10:18:55 +0500

    iniitial commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
-let elshopbox=document.getElementById('modalshop');
-let elshopbtn=document.getElementById('btnshop');
-let elshopremove=document.getElementById('shopbtn');
+let eloff1modal=document.getElementById('off1modal');
+let eloff1open=document.getElementById('off1open');
+let eloff1remove=document.getElementById('off1remove');
 
-elshopbtn.addEventListener('click',function () {
-elshopbox.classList.add('show');
+eloff1open.addEventListener('click',function () {
+eloff1modal.classList.add('show');
 });
 
-elshopremove.addEventListener('click',function () {
-elshopbox.classList.remove('show');
+eloff1remove.addEventListener('click',function () {
+eloff1modal.classList.remove('show');
+ });
+
+
+
+let eloff2modal=document.getElementById('off2modal');
+let eloff2open=document.getElementById('off2open');
+let eloff2remove=document.getElementById('off2remove');
+
+eloff2open.addEventListener('click',function () {
+eloff2modal.classList.add('show');
+});
+
+eloff2remove.addEventListener('click',function () {
+eloff2modal.classList.remove('show');
+ });
+
+
+
+
+ let eloff3modal=document.getElementById('off3modal');
+let eloff3open=document.getElementById('off3open');
+let eloff3remove=document.getElementById('off3remove');
+
+eloff3open.addEventListener('click',function () {
+eloff3modal.classList.add('show');
+});
+
+eloff3remove.addEventListener('click',function () {
+eloff3modal.classList.remove('show');
  });
